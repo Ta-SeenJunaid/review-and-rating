2aaf7328202dbd5164b8d4ae13928c60e6420105
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-12-28 22:37:43 +0600

    Project Initialization

---
+++ review-and-rating/src/main/java/com/teamU/groupFour/reviewandrating/ReviewAndRatingApplication.java
@@ -0,0 +1,13 @@
+package com.teamU.groupFour.reviewandrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAndRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAndRatingApplication.class, args);
+	}
+
+}
---
+++ review-and-rating/src/test/java/com/teamU/groupFour/reviewandrating/ReviewAndRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamU.groupFour.reviewandrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAndRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
